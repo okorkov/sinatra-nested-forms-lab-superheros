9a584ff15671328c2f3636cdaa150b1c67e089c0
okorkov <alex.okorkov@gmail.com>
2020-12-16 13:44:48 -0800

    commit tree fbe05f30cc9d629609c34a56ccbc5958aec58d32

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,20 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :super_hero
+    end
+
+    post '/teams' do
+        @params = params
+        @team = params[:team]
+        @heroes = params[:team][:members]
+        erb :team
+    end
 
 end
+
+# {"team"=>{"name"=>"asdasd", "motto"=>"dadsasd", "members"=>
+#     [{"name"=>"dadsad", "power"=>"dadsasd", "biography"=>"dadssad"}, 
+#         {"name"=>"dadad", "power"=>"dada", "biography"=>"d"}, 
+#         {"name"=>"dasdasdad", "power"=>"dasda", "biography"=>"dadsdsd"}]}}
